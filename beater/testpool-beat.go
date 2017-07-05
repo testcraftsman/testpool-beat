61b5ec69acfe59387f21c9a091e8679823e5e7a5
Mark Hamilton <mark.lee.hamilton@gmail.com>
2017-07-04 20:26:33 -0700

    moved profile generator outside of testpool-beat interface.

--- beater/testpool-beat.go
+++ beater/testpool-beat.go
@@ -1,11 +1,8 @@
 package beater
 
 import (
-    "bufio"
 	"fmt"
 	"time"
-    "os"
-    "encoding/json"
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
@@ -35,52 +32,6 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 		config: config,
 	}
 	return bt, nil
-}
-
-type Message struct {
-    Profile   string    `json: "profle"`
-    Level     string    `json: "level"`
-    Vm_max    int       `json: "vm_max"`
-    Vm_count  int       `json: "vm_count"`
-    Timestamp time.Time `json: "RFC3339Nano"`
-}
-
-
-// profileRead: Read profile log content.
-// Follows generator pattern by returning a channel.
-func profilRead(profile_path string) (<-chan Message, error) {
-
-  fhndl, err := os.Open(profile_path)
-  if err != nil {
-    return nil, err
-  }
-
-  generator := make (chan Message)
-  scanner := bufio.NewScanner(fhndl)
-
-  go func () {
-    // Defer must be in here because entering go routine will
-    // cause defer.
-    defer fhndl.Close()
-
-    for scanner.Scan() {
-      data := []byte(scanner.Text())
-      var msg Message
-
-      err := json.Unmarshal(data, &msg)
-  
-      if err == nil {
-        generator <- msg
-      }
-    }
-    close(generator)
-  }()
-   
-  if err := scanner.Err(); err != nil {
-    return nil, err
-  }
-
-  return generator, nil
 }
 
 func (bt *TestpoolBeat) Run(b *beat.Beat) error {
